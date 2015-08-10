06078794e0ed875f031f19333b361ad4cc145f25
Antonio Vicente Martin <antoniovicente.avm@gmail.com>
2015-08-10 22:09:19 +0200

    Implement play event to create morse playback

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 	var tfString = document.getElementById('string');
 	var tfMorse = document.getElementById('morse');
+	var bPlay = document.getElementById('b-play');
 
 	tfString.onkeyup = function(event){
 		var morse = Morse.toMorse(event.srcElement.innerText);
@@ -11,6 +12,11 @@
 	tfMorse.onkeyup = function(event){
 		var string = Morse.fromMorse(event.srcElement.innerText);
 		tfString.textContent = string;
+	};
+
+	bPlay.onclick = function(event){
+		var morse = tfMorse.innerText;
+		Audio.play(morse);
 	};
 
 	// Init
